2b2f9602835009e558ed8e8b1f26210b94fe4fe6
Dipesh Saxena <59689186+Dipesh23@users.noreply.github.com>
2024-01-24 03:37:20 +0530

    adding header style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,43 @@
-import React from 'react'
-import { FaSearch, FaShoppingBag } from 'react-icons/fa'
+
+import { useState } from 'react';
+import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
+const user = {_id:"dkjfdjf",role:"admin"};
+
 const Header = () => {
+    const [isOpen,setisOpen]=useState<boolean>(false)
+    const logoutHandler = ()=>{
+        setisOpen(false);
+    }
   return (
-   <nav>
-    <Link to={"/"}>Home</Link>
-    <Link to={"/search"}><FaSearch/></Link>
-    <Link to={"/cart"}><FaShoppingBag/></Link>
+   <nav className='header'>
+    <Link onClick={()=> setisOpen(false)} to={"/"}>Home</Link>
+    <Link onClick={()=> setisOpen(false)} to={"/search"}><FaSearch/></Link>
+    <Link onClick={()=> setisOpen(false)} to={"/cart"}><FaShoppingBag/></Link>
+
+    {
+        user?._id?(
+            <>
+            <button onClick={()=> setisOpen((prev)=> !prev)}>
+                <FaUser/>
+            </button>
+
+            <dialog open={isOpen}>
+                <div>
+                    {
+                        user.role==="admin" && (
+                            <Link onClick={()=> setisOpen(false)} to="/admin/dashboard" >Admin</Link>
+                        )
+                    }
+                    <Link onClick={()=> setisOpen(false)} to="/orders">Orders</Link>
+                    <button onClick={logoutHandler} ><FaSignOutAlt/></button>
+                </div>
+            </dialog>
+            
+            </>
+        ):(<Link to={"/login"}><FaSignInAlt/></Link>)
+    }
    </nav>
   )
 }
