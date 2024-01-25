ad6571b511674eaeb875db98dbee97e39f7de68a
Dipesh Saxena <59689186+Dipesh23@users.noreply.github.com>
2024-01-25 23:57:39 +0530

    product-card

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+
+import { Link } from 'react-router-dom'
+import ProductCard from '../components/product-cart'
 
 const Home = () => {
+  const addToCartHandler =()=>{}
   return (
-    <div>
-      Home
+    <div className='home'>
+      <section>
+
+      </section>
+
+      <h1>Latest Products
+      <Link to="/search" className='findmore'>More</Link>
+      </h1>
+      
+      <main>
+        <ProductCard productId='adsssf'
+         name='macbook'
+         price={1666} 
+         stock={22}
+         handler={addToCartHandler}
+         photo="https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg"
+         />
+
+      </main>
     </div>
   )
 }
